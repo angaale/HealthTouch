e1bee03a3a5992b0e65fe7a00dbcc999723ef1f6
Anastasia Golenyaeva <n.golenyaeva@gmail.com>
2024-07-05 16:52:41 +0300

    Добавлен Onboarding Screen в трех состояниях

---
+++ app/src/main/java/com/golenyaeva/healthtouch/screen/onboardingscreen/OnboardingUIModel.kt
@@ -0,0 +1,11 @@
+package com.golenyaeva.healthtouch.screen.onboardingscreen
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class OnboardingUIModel(
+    @DrawableRes val imageRes: Int? = null,
+    @StringRes val title: Int? = null,
+    @StringRes val description: Int? = null,
+    val currentPage: Int = 1,
+)
