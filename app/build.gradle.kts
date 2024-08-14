5b4476753f0270d7615c1ffd1dc5e98c4118e38d
Anastasia Golenyaeva <n.golenyaeva@gmail.com>
2024-08-14 12:31:53 +0300

    Добавлен подсчет пульса по изображению с камеры

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.camera.lifecycle)
     implementation(libs.camera.view)
     implementation(libs.camera.extensions)
+    implementation(libs.palette)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/golenyaeva/healthtouch/presentation/screen/measure/MeasureScreen.kt
+++ app/src/main/java/com/golenyaeva/healthtouch/presentation/screen/measure/MeasureScreen.kt
@@ -2,8 +2,12 @@ package com.golenyaeva.healthtouch.presentation.screen.measure
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.LinearLayout
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
@@ -15,7 +19,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -28,7 +34,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import androidx.palette.graphics.Palette
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -41,8 +49,11 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executor
 
-@SuppressLint("PermissionLaunchedDuringComposition")
+@SuppressLint("PermissionLaunchedDuringComposition", "CoroutineCreationDuringComposition")
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MeasureScreen(
@@ -54,6 +65,12 @@ fun MeasureScreen(
     )
     val cameraPermissionState: PermissionState =
         rememberPermissionState(android.Manifest.permission.CAMERA)
+
+    val composableScope = rememberCoroutineScope()
+
+    val pulseState = remember {
+        mutableIntStateOf(0)
+    }
 
     if (!cameraPermissionState.status.isGranted) {
         Box(modifier = Modifier.fillMaxSize()) {
@@ -88,6 +105,35 @@ fun MeasureScreen(
         }
     )
 
+    val mainExecutor: Executor = ContextCompat.getMainExecutor(context)
+
+    val colors = mutableListOf<Int>()
+    composableScope.launch {
+        val time = 100L
+        delay(1000L)
+        while (true) {
+            cameraController.takePicture(
+                mainExecutor,
+                object : ImageCapture.OnImageCapturedCallback() {
+                    override fun onCaptureSuccess(image: ImageProxy) {
+                        val bitmap: Bitmap = image.toBitmap()
+                        Palette.from(bitmap).generate { palette ->
+                            val color = palette?.getVibrantColor(Color.WHITE)
+                            println("Calculated color: $color")
+                            if (color != null) {
+                                colors.add(color)
+                                val pulse = calculatePulse(colors, time)
+                                pulseState.intValue = pulse.toInt()
+                                println("Calculated Pulse: $pulse BPM")
+                            }
+                        }
+                        image.close()
+                    }
+                })
+            delay(time)
+        }
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize(),
@@ -104,7 +150,7 @@ fun MeasureScreen(
                 fontWeight = FontWeight(400),
                 text = stringResource(
                     id = R.string.measure_screen_bpm,
-                    state.pulse
+                    pulseState.intValue
                 )
             )
             LottieAnimation(
@@ -142,7 +188,28 @@ fun MeasureScreen(
             )
         }
     }
+}
 
+fun detectPeaks(data: List<Int>): List<Int> {
+    val peaks = mutableListOf<Int>()
+    for (i in 1 until data.size - 1) {
+        if (data[i] < data[i - 1] && data[i] < data[i + 1]) {
+            peaks.add(i)
+        }
+    }
+    return peaks
+}
+
+fun calculatePulse(colorData: List<Int>, time: Long): Double {
+    val peaks = detectPeaks(colorData)
+
+    val intervals = mutableListOf<Double>()
+    for (i in 1 until peaks.size) {
+        val interval = (peaks[i] - peaks[i - 1]).toDouble() * time
+        intervals.add(interval)
+    }
+    val averageInterval = intervals.average()
+    return 60000 / averageInterval
 }
 
 @Preview
